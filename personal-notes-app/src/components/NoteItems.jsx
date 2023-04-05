2b7bfbfc903070d01b6d8f9f1f8ed4b006af17e8
mamantube <firmanhiday.id007@gmail.com>
2023-04-05 13:44:11 +0700

    try

--- personal-notes-app/src/components/NoteItems.jsx
+++ personal-notes-app/src/components/NoteItems.jsx
@@ -1,4 +1,8 @@
 import React from "react";
 import { showFormattedDate } from "../utils/data";
 
-export default function NoteItems({note. actionButtonText, onMarkNote})
+function NoteItems ({note, onDelete, onArchive}) {
+    return (
+        <div className="note-item"></div>
+    )
+}
